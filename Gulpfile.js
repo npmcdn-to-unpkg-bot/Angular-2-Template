c531958c3d7e4d0b63ae26304aed15f577ec1941
tcozzens <tcozzens91@gmail.com>
2016-05-21 13:09:27 -0400

    Initial Angular 2 Commit

---
+++ Gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src(['./**/*.scss', './*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(function(file){
+            return file.base;
+        }));
+});
+
+gulp.task('default',function() {
+    gulp.watch(['./**/*.scss', './*.scss'],['styles']);
+}); 
---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular 2 App</h1><h2>d00d</h2>',
+  styleUrls: ['app/app.component.scss']
+})
+export class AppComponent { }
